a7c52a90baac9fe7bb874fd6c76d146873d9f9c4
justascerk <justas.cerkauskas@gmail.com>
2020-02-10 20:15:26 +0200

    pridetas failas

---
+++ Skaicsuma.java
@@ -0,0 +1,24 @@
+package Skaiciusuma;
+
+import java.util.Scanner;
+
+public class Skaicsuma {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Iveskite skaiciu: ");
+		int n;
+		if ( scanner.hasNextInt()) {
+			 n = scanner.nextInt();
+			 int sum = 0;
+			for (int i = 1; i < n; i++) {
+				System.out.print(i + "+");
+				sum += i;
+			}
+			sum += n;
+			System.out.print(n + "=" + sum);
+		} else {
+			System.out.println("Netinkamas ivedimas");
+		}
+	}
+}
